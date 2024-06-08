6a6418df1e059250b977f48fe3065111bcae24f7
Diarmuid Lloyd <diarmuid.lloyd@gmail.com>
2024-06-08 21:19:56 +0100

    Build basic table

--- 02_build_table_gt.R
+++ 02_build_table_gt.R
@@ -5,9 +5,22 @@ library(gtExtras)
 
 source("01_spfl_collect_data.R")
 
-spfl_table_gt <- 
-  spfl_table |> 
-select(team, url, outcomes)
+spfl_table_gt <-
+  spfl_table |>
+  select(position,
+    team,
+    url,
+    ht_goals,
+    ft_goals,
+    corners,
+    reds,
+    yellows,
+    game_lost_FT_from_non_losing_HT,
+    game_won_FT_from_non_win_HT ,
+    outcomes,
+    prop_games_list,
+    total_points
+  )
 
 # Ideas
 # win/loss/draw sparkline
@@ -26,16 +39,30 @@ spfl_table_gt |>
     }
   ) |> 
   
-  gtExtras::gt_plt_winloss(outcomes, type = "pill", width = 38, max_wins = 38) |> 
+  gtExtras::gt_plt_winloss(outcomes, type = "pill", width = 38, max_wins = 38,palette = c("#28A197","#F46A25","#3D3D3D")) |> 
+  
+  gtExtras::gt_plt_bar_stack(prop_games_list, labels = c("Won","Drawn","Lost"), fmt_fn = scales::percent_format(trim = TRUE), palette = c("#28A197","#3D3D3D","#F46A25"), width = 50) |> 
+  
+  gt_highlight_cols(
+    reds,
+    fill = "#FF0000",
+    alpha = 0.2,
+    font_weight = "normal",
+    font_color = "#000000"
+  ) |> 
+  
   
   tab_header(
     title = gtExtras::add_text_img(
-      "2023–24 Scottish Premiership",
+      "A season in numbers: 2023–24 Scottish Premiership",
       url = "https://upload.wikimedia.org/wikipedia/en/a/a1/Cinch_premiership.png",
       height = 30
     )
   ) |> 
   
+  opt_table_font(stack = "geometric-humanist") |>
+  
+
   opt_interactive(
     
     active = TRUE,
